9206dc1a3093a75e6ef6ef9739349b017a3fca7a
Pedro Patriota <pedro.patriota_storm@prestador.globo>
2021-02-14 22:06:03 -0300

    RTL

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,7 @@
+const Main = () => (
+  <main>
+    <h2>react avançado</h2>
+  </main>
+)
+
+export default Main
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,4 @@
 export default function Home() {
-  const foo = "bla"
-
   return (
     <div>
       <h1>React</h1>
